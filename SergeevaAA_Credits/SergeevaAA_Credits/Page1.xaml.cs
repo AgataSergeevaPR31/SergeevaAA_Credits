431c9340ac242f5124b0f164665dc0b27ca574f1
AgataSergeevaPR31 <agatasergeeva60@gmail.com>
2024-12-02 14:14:21 +0500

    Добавьте файлы проекта.

---
+++ SergeevaAA_Credits/SergeevaAA_Credits/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SergeevaAA_Credits
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void DDDD(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Login.Text) ||
+                string.IsNullOrWhiteSpace(Password.Text) ||
+                !checkBox.IsChecked)
+            {
+                await DisplayAlert("Ошибка", "Пожалуйста, заполните все поля и выберите 'Запомнить меня'.", "OK");
+            }
+            else
+            {
+                await Navigation.PushModalAsync(new Page2());
+            }
+        }
+    }
+
+    
+    
+
+}
---
+++ SergeevaAA_Credits/SergeevaAA_Credits/Page3.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SergeevaAA_Credits
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page3 : ContentPage
+	{
+		public Page3 ()
+		{
+			InitializeComponent();
+		}
+
+        public void RefreshData()
+        {
+            DateTime currentDate = DateTime.Now;
+            CurrentDateLabel.Text = currentDate.ToString("dd MMMM yyyy");
+        }
+    }
+}
